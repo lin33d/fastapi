d26193b3a5b5813e81f5dba184ec1473be9bd219
lin33d <fishchev.den@mail.ru>
2025-04-28 20:17:49 +0400

    Валидация данных

--- Booking/app/main.py
+++ Booking/app/main.py
@@ -1,3 +1,47 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query, Depends
+from typing import Optional
+from datetime import date
+from pydantic import BaseModel
 
 app = FastAPI()
+
+
+class SHotel(BaseModel):
+    adress: str
+    name: str
+    stars: int
+
+
+class HotelSearchArgs():
+    def __init__(
+        self,
+        location: str,
+        date_from: date,
+        date_to: date,
+        stars: Optional[int] = Query(None, ge=1, le=5),
+        has_spa: Optional[bool] = None,
+                 
+    ):
+        self.location = location
+        self.date_from = date_from 
+        self.date_to = date_to 
+        self.stars = stars 
+        self.has_spa = has_spa 
+
+
+@app.get("/hotels", response_model=list[SHotel])
+def get_hotels(
+    search_args: HotelSearchArgs = Depends()              
+    ):
+
+    return search_args
+
+
+class SBooking(BaseModel):
+    room_id: int
+    date_from: date 
+    date_to: date
+
+@app.post("/bookings")
+def add_booking(booking: SBooking):
+    pass
