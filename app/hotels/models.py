924dc8a4382b50ef23602a96ce6091ed97193130
lin33d <fishchev.den@mail.ru>
2025-05-03 19:38:45 +0400

    Папка 5. База данных. Подключение закончена

--- app/hotels/models.py
+++ app/hotels/models.py
@@ -8,5 +8,5 @@ class Hotels(Base):
     name = Column(String, nullable = False) 
     location = Column(String, nullable = False) 
     services = Column(JSON)
-    room_quantity = Column(Integer, nullable = False)
+    rooms_quantity = Column(Integer, nullable = False)
     image_id = Column(Integer) 
