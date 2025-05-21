4080b0b428a106017d56e3f0975671d4368806a4
lin33d <fishchev.den@mail.ru>
2025-05-21 15:10:26 +0400

    Router

---
+++ app/bookings/router.py
@@ -0,0 +1,19 @@
+from fastapi import APIRouter
+from sqlalchemy import select
+
+from app.database import async_session_maker
+from app.bookings.models import Bookings
+
+
+router = APIRouter(
+    prefix="/bookings",
+    tags=["Bookings"],
+)
+
+@router.get("")
+async def get_bookings():
+    async with async_session_maker() as session:
+        query = select(Bookings) #SELECT * FROM bookings;
+        result = await session.execute(query)
+        print(result.all())
+
